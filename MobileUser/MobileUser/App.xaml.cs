616e59658bb6f475464edfbb21502681fc2955b7
willian xavier <wlrxavier@gmail.com>
2022-02-08 12:04:44 -0300

    Add httpclient and api url to App class

--- MobileUser/MobileUser/App.xaml.cs
+++ MobileUser/MobileUser/App.xaml.cs
@@ -3,25 +3,40 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using System.Net.Http;
 using System.Net.Http.Headers;
-
+using Xamarin.Essentials;
 
 namespace MobileUser
 {
     public partial class App : Application
     {
-        public HttpClient httpClient;
-
         public App()
         {
             InitializeComponent();
 
-            HttpClientHandler insecureHandler = GetInsecureHandler();
-            httpClient = new HttpClient(insecureHandler);
-            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            string apiBaseAddress = DeviceInfo.Platform == DevicePlatform.Android ? "https://10.0.2.2" : "https://localhost";
+            string apiHttpsPort = "8001";
+            string apiRoute = "users";
+            
+            string apiUrl = $"{apiBaseAddress}:{apiHttpsPort}/{apiRoute}";
 
-            MainPage = new MainPage(httpClient);
+            HttpClient httpClient = GetHttpClient();
+
+            MainPage = new MainPage(httpClient, apiUrl);
+        }
+
+        public HttpClient GetHttpClient()
+        {
+            HttpClientHandler insecureHandler = GetInsecureHandler();
+
+            HttpClient client = new HttpClient(insecureHandler);
+
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+
+            return client;
         }
         
+        // bypass used only for development purposes,
+        // mobile dont permit self signed certificates
         public HttpClientHandler GetInsecureHandler()
         {
             HttpClientHandler handler = new HttpClientHandler();
