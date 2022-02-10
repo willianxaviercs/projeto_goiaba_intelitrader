950f705b0bef37f0061be6cb2724954511f4e9de
willian xavier <wlrxavier@gmail.com>
2022-02-10 13:16:55 -0300

    Add User Api Model

--- MobileUser/MobileUser/User.cs
+++ MobileUser/MobileUser/User.cs
@@ -1,17 +1,17 @@
 using System;
-//using System.ComponentModel.DataAnnotations;
 
 namespace MobileUser
 {
-    public class User
+public class User
     {
-  //      [Required]
         public string FirstName  { get; set; }
         
         public string SurName { get; set; }
 
-    //    [Range(1, 128)]
-      //  [Required]
         public int Age { get; set; }
+
+        public DateTime CreationTime { get; set; }
+
+        public Guid Id { get; set; }
     }
 }
